a2a9f5521448f1b5e2d72f98223facae3b3fa3bd
JK3Associatesinc <jk3halton@outlook.com>
2025-06-15 11:34:21 -0400

    Added App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./Menu";
+import ARView from "./ARView";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/ar/:id" element={<ARView />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
